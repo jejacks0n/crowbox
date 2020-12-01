5d6cae06c5a867fd346d7f3291e2388976d1627a
jejacks0n <jjackson@gitlab.com>
2020-12-01 15:52:09 -0700

    Improve server behavior

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const {Server} = require('ws');
 const PORT = process.env.PORT || 3000;
 
 const server = express()
-  .head('/event', ({query}) => {
-    if (process.env.API_TOKEN !== query['token']) return;
-    wss.clients.forEach((client) => client.send(query['type'] || -1));
+  .head('/event', ({query}, res) => {
+    if (process.env.API_TOKEN === query['token']) {
+      wss.clients.forEach((client) => client.send(query['type'] || -1));
+    }
+    res.sendStatus(204);
   })
   .use((req, res) => res.sendFile('/index.html', {root: __dirname}))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
